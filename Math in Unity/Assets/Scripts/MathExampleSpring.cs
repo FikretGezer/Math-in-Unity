a731af7030bcf45b4c1f84ed7e35e2f39d608a7c
Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-09-15 23:41:20 +0300

    code improvements

--- Math in Unity/Assets/Scripts/MathExampleSpring.cs
+++ Math in Unity/Assets/Scripts/MathExampleSpring.cs
@@ -8,6 +8,8 @@ public class MathExampleSpring : MonoBehaviour
 {
     const float TAU = 6.28318530718f;
     public bool firstWay;
+    public AnimationCurve _curve;
+    public Gradient _gradient;
 
     [Header("First Way Parameters")]
     [Range(0, 2f)] public float radius = 0.1f;
@@ -20,9 +22,18 @@ public class MathExampleSpring : MonoBehaviour
     [Range(0, 2f)] public float radiusSecond = 0.1f;
     [Range(16, 256)]public float detail2 = 64;
     public float height = 1f;
+    public float lerpSpeed = 1f;
+    private float current, target;
+    void Awake()
+    {
+        current = 0;
+    }
     void OnDrawGizmos()
     {
         Gizmos.matrix = Handles.matrix = transform.localToWorldMatrix;
+
+
+
         //First way of doing it
         if(firstWay)
         {
@@ -42,10 +53,21 @@ public class MathExampleSpring : MonoBehaviour
         }
         else //Second way of doing it
         {
+            //if(current == target) target = current == 0f ? 10f : 0f;
+            target = 10f;
+            if(current == 10f)
+            {
+                current = 0f;
+            }
+            current = Mathf.MoveTowards(current, target, lerpSpeed * Time.deltaTime);
+
             Vector3 prev = GetPointSecond(0);
             for (int i = 1; i < detail2; i++)
             {
                 float t = i / (detail2 - 1f);
+                //_curve.Evaluate(t);
+                Gizmos.color = _gradient.Evaluate(t);
+                
                 Vector3 cur = GetPointSecond(t);
                 Gizmos.DrawLine(prev, cur);
                 prev = cur;
@@ -59,7 +81,7 @@ public class MathExampleSpring : MonoBehaviour
     }
     Vector3 GetPointSecond(float t)
     {
-        float angle = t * TAU * turnCount;
+        float angle = t * TAU * current;
         Vector2 dir = AngleToDir(angle) * radiusSecond;
         return new Vector3(dir.x, t * height, dir.y);
     }
